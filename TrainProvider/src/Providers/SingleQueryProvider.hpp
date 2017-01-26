eb7e58df39b04ba57b1fd6d645e6251098ecf619
Valentin Rosca <rosca.valentin2012@gmail.com>
2017-01-26 04:27:26 +0200

    Relocated SingleQuery to src
    Signed-off-by: Valentin Rosca <rosca.valentin2012@gmail.com>

---
+++ TrainProvider/src/Providers/SingleQueryProvider.hpp
@@ -0,0 +1,30 @@
+//
+// Created by vrosca on 1/21/17.
+//
+
+#ifndef FASTTRAINSERVICE_SQPROVIDER_HPP
+#define FASTTRAINSERVICE_SQPROVIDER_HPP
+
+#include <json/json.hpp>
+#include <string>
+#include <DatabaseManager.hpp>
+#include <vector>
+#include <DatabaseUtils.hpp>
+#include <Logger.hpp>
+#include "Provider.hpp"
+
+using json = nlohmann::json;
+
+class SingleQueryProvider : public Provider {
+protected:
+    std::string query;
+    virtual json provide(neo4j_result_stream_t *result_stream) {
+        return json ();
+    }
+public:
+    std::string type;
+    json execute ();
+};
+
+
+#endif //FASTTRAINSERVICE_SQPROVIDER_HPP
